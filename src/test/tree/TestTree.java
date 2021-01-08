43d2a047db45afe54cbe1d734ffa785ab71fcb0c
Drimal Jiri <st64297@upce.cz>
2021-01-08 09:56:40 +0100

    final commit

--- src/test/tree/TestTree.java
+++ src/test/tree/TestTree.java
@@ -1,5 +1,6 @@
 package tree;
 
+import calendar.Calendar;
 import calendar.Event;
 import calendar.EventSet;
 import calendar.Tree;
@@ -16,7 +17,9 @@ import static org.junit.Assert.*;
 public class TestTree {
     private Tree<EventSet> emptyTree;
     private Tree<EventSet> tree;
-    private EventSet eventSet = new EventSet(new Date(117, 6, 1));
+    private Tree<EventSet> extraTree;
+    private EventSet eventSet1 = new EventSet(new Date(117, 6, 1));
+    private EventSet eventSet2 = new EventSet(new Date(117, 6, 2));
 
     @BeforeClass
     public static void setUpClass() {
@@ -31,8 +34,11 @@ public class TestTree {
     public void setUp() {
         emptyTree = new Tree<>();
         tree = new Tree<>();
+        tree.add(eventSet1);
 
-        tree.add(eventSet);
+        extraTree = new Tree<>();
+        extraTree.add(eventSet1);
+        extraTree.add(eventSet2);
     }
 
     @After
@@ -47,8 +53,46 @@ public class TestTree {
 
     @Test
     public void testAddingToEmptyTree() {
-        emptyTree.add(eventSet);
+        emptyTree.add(eventSet1);
         assertNotEquals(emptyTree, tree);
     }
+
+    @Test
+    public void testRemoveRoot() {
+        extraTree.erase(eventSet1);
+        assertEquals(tree,extraTree);
+    }
+
+    @Test
+    public void testRemoveList() {
+        extraTree.erase(eventSet2);
+        assertEquals(tree,extraTree);
+    }
+
+    @Test
+    public void testIterateOnEmptyTree() {
+        assertFalse(emptyTree.iterator().hasNext());
+    }
+
+    @Test
+    public void testIterateOnNotEmptyTree() {
+        assertTrue(tree.iterator().hasNext());
+    }
+
+    @Test
+    public void testCalendarDeleteAll() {
+        Date testDate = new Date(117, 6, 1);
+        Calendar calendar = new Calendar();
+        calendar.addEvent(new Event(new Date(117, 6, 1), "1-1"));
+        calendar.addEvent(new Event(new Date(117, 6, 2), "2-1"));
+        calendar.addEvent(new Event(new Date(117, 6, 3), "3-1"));
+        calendar.addEvent(new Event(new Date(117, 6, 1), "1-2"));
+        calendar.addEvent(new Event(new Date(117, 6, 1), "1-3"));
+        calendar.addEvent(new Event(new Date(117, 6, 1), "1-4"));
+
+        calendar.DeleteAll(testDate);
+        assertEquals(null,calendar.eventsList(testDate));
+    }
+
 
 }
